32b379bc71df6910526e2dd4ec41cdc755bc5e10
erduoya <erduoya@gmail.com>
2024-12-02 16:45:59 +0800

    整合

--- themes/hexo-erduo/index.js
+++ themes/hexo-erduo/index.js
@@ -34,7 +34,7 @@ import TocDrawerButton from './components/TocDrawerButton'
 import CONFIG from './config'
 import { Style } from './style'
 import BlogMemos from './components/BlogMemos'
-import AISummary from './components/AISummary'
+
 
 const AlgoliaSearchModal = dynamic(
   () => import('@/components/AlgoliaSearchModal'),
